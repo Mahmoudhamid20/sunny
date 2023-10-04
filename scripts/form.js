4b34e33e2d07cec422f7b21e9107b9a8128842db
artygg <artjoms.grisajevs@student.nhlstenden.com>
2023-10-04 10:08:24 +0200

    Add: Prevent of form submission if form wasn't validated

--- scripts/form.js
+++ scripts/form.js
@@ -1,17 +1,5 @@
 $(document).ready(function () {
-    let checks = {"Name": false, "Surname": false, "Email": false, "Message": false,};
-     $("#Name").change(function () {
-        validate("Name");
-     });
-    $("#Surname").change(function () {
-        validate("Surname");
-    });
-    $("#Email").change(function () {
-        validate("Email");
-    });
-    $("#Message").change(function () {
-        validate("Message");
-    });
+    let checks = {"Name": true, "Surname": true, "Email": true, "Message": true};
     function validate(id) {
         let input = $("#"+id);
         if ((input.val().replace(/\s/g, '').length === 0) || (id === "Email" && !input.val().includes("@"))) {
@@ -26,4 +14,35 @@ $(document).ready(function () {
             $("#warning-"+id).hide(200);
         }
     }
-})
+
+
+    $("#Name").change(function () {
+        validate("Name");
+    });
+    $("#Surname").change(function () {
+        validate("Surname");
+    });
+    $("#Email").change(function () {
+        validate("Email");
+    });
+    $("#Message").change(function () {
+        validate("Message");
+    });
+
+    $("form").submit(function(event) {
+        let ids = ["Name", "Surname", "Email", "Message"];
+        ids.forEach((id) => validate(id));
+        for (let k in checks){
+            if(checks[k] === true) {
+                event.preventDefault();
+                return false;
+            }
+        }
+        return true;
+
+    });
+
+
+
+
+});
