630d6b71071c83f66a079431502e8f1daaeb4582
artygg <artjoms.grisajevs@student.nhlstenden.com>
2023-11-01 12:24:04 +0100

    Add: favicon

--- contact.php
+++ contact.php
@@ -5,10 +5,12 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="icon" type="image/png" href="/img/Logo/png/favicon.png">
     <link rel="stylesheet" href="styles/style.css">
     <link rel="stylesheet" href="styles/contact.css">
     <title>Contact us</title>
     <script src="scripts/jquery-3.7.1.min.js"></script>
+
 </head>
 <body>
 <header></header>
